c9edd30e3f5e0123beb2526b53ef696cc3dcf456
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-10-18 09:38:43 +0700

    bai tap ss3

---
+++ ss3/exercise/exercise_2/exercise_2/src/components/Todo.js
@@ -0,0 +1,51 @@
+'use strict';
+import React, {Component} from "react";
+
+class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [],
+            item: "",
+        };
+    };
+
+    handleChange = even => {
+        this.setState({item: even.target.value});
+    }
+
+    handleAddItem = () => {
+        if (this.state.item) {
+            this.setState({
+                ...this.state,
+                list: [...this.state.list, this.state.item]
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <center>
+                    <div>
+                        <h2>Todo list</h2>
+                    </div>
+                    <div>
+                        <input onChange={(e) => this.handleChange(e)} type="text" style={{width: 200}}/>
+                        <button onClick={this.handleAddItem}>Add</button>
+                    </div>
+                    <div>
+                        <h3>Added items</h3>
+                        <ul>
+                            {
+                                this.state.list.map((item) => <li>{item}</li>)
+                            }
+                        </ul>
+                    </div>
+                </center>
+            </div>
+        )
+    }
+}
+
+export default Todo;
