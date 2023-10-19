b98c97e07649aeb37d21b79a0c120e41c239ac7d
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-10-19 08:39:25 +0700

    fix bai tap

--- ss2/exercise/exercise_1/exercise_1/src/index.js
+++ ss2/exercise/exercise_1/exercise_1/src/index.js
@@ -3,31 +3,37 @@ import ReactDOM from "react-dom/client";
 
 const students = [
     {
+        id: 1,
         company: 'Alfreds Futterkiste',
         contact: 'Maria Anders',
         country: 'Germany'
     },
     {
+        id: 2,
         company: 'Centro comercial Moctezuma',
         contact: 'Francisco Chang',
         country: 'Mexico'
     },
     {
+        id: 3,
         company: 'Ernst Handel',
         contact: 'Roland Mendel',
         country: 'Austria'
     },
     {
+        id: 4,
         company: 'Island Trading',
         contact: 'Helen Bennett',
         country: 'UK'
     },
     {
+        id: 5,
         company: 'Laughing Bacchus Winecellars',
         contact: 'Yoshi Tannamuri',
         country: 'Canada'
     },
     {
+        id: 6,
         company: 'Magazzini Alimentari Riuniti',
         contact: 'Giovanni Rovelli',
         country: 'Italy'
@@ -40,13 +46,22 @@ root.render(
     <div>
         <h1>Student Manager</h1>
         <table>
+            <thead>
+            <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+            </thead>
+            <tbody>
             {students.map((student) => (
-                <tr>
+                <tr key={student.id}>
                     <td>{student.company}</td>
                     <td>{student.contact}</td>
                     <td>{student.country}</td>
                 </tr>
             ))}
+            </tbody>
         </table>
     </div>
 );
